a47314b8d7344ad14577d217c8a02ddbbaa4c78a
Mark Sherriff <marksherriff@github.com>
2017-02-28 13:27:21 -0500

    v. 1.0 - all features there.

--- Core Skills/CoreDataViewController.swift
+++ Core Skills/CoreDataViewController.swift
@@ -7,13 +7,92 @@
 //
 
 import UIKit
+import CoreData
 
 class CoreDataViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var compIDTextField: UITextField!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var compIDLabel: UILabel!
+    
+    @IBAction func saveData(_ sender: UIButton) {
+        
+        guard let appDelegate =
+            UIApplication.shared.delegate as? AppDelegate else {
+                return
+        }
+        
+        let context =
+            appDelegate.persistentContainer.viewContext
+        
+        //retrieve the entity that we just created
+        let entity =  NSEntityDescription.entity(forEntityName: "Person", in: context)!
+        
+        let transc = NSManagedObject(entity: entity, insertInto: context)
+        
+        //set the entity values
+        transc.setValue(nameTextField.text!, forKey: "name")
+        transc.setValue(compIDTextField.text!, forKey: "compID")
+        
+        //save the object
+        do {
+            try context.save()
+            print("saved!")
+        } catch let error as NSError  {
+            print("Could not save \(error), \(error.userInfo)")
+        } catch {
+            
+        }
+        
+    }
+    
+    
+    @IBAction func loadData(_ sender: UIButton) {
+        
+        let fetchRequest: NSFetchRequest<Person> = Person.fetchRequest()
+        
+        guard let appDelegate =
+            UIApplication.shared.delegate as? AppDelegate else {
+                return
+        }
+        
+        let context =
+            appDelegate.persistentContainer.viewContext
+        
+        do {
+            //go get the results
+            let searchResults = try context.fetch(fetchRequest)
+            
+            print ("num of results = \(searchResults.count)")
+            
+            //You need to convert to NSManagedObject to use 'for' loops
+            for person in searchResults as [NSManagedObject] {
+                
+                print("\(person.value(forKey: "compID"))")
+                self.compIDLabel.text = person.value(forKey: "compID") as! String?
+                self.nameLabel.text = person.value(forKey: "name") as! String?
+
+            }
+        } catch {
+            print("Error with request: \(error)")
+        }
+
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(CoreDataViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+        
+        
         // Do any additional setup after loading the view.
+    }
+    
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,14 +101,5 @@ class CoreDataViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+   
 }
--- Core Skills/UserDefaultsViewController.swift
+++ Core Skills/UserDefaultsViewController.swift
@@ -9,11 +9,39 @@
 import UIKit
 
 class UserDefaultsViewController: UIViewController {
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var compIDTextField: UITextField!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var compIDLabel: UILabel!
+    @IBAction func saveData(_ sender: UIButton) {
+        
+        UserDefaults.standard.set(nameTextField.text, forKey: "name")
+        UserDefaults.standard.set(compIDTextField.text, forKey: "compID")
+    }
+    @IBAction func loadData(_ sender: UIButton) {
+        let name = UserDefaults.standard.string(forKey: "name")
+        let compID = UserDefaults.standard.string(forKey: "compID")
+        
+        nameLabel.text = name!
+        compIDLabel.text = compID!
+    
+        
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UserDefaultsViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+        
+        
         // Do any additional setup after loading the view.
+    }
+    
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +49,5 @@ class UserDefaultsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
